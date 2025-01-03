8807a1ad6f254b3c5a0e3dbafdc96f091b8447ae
승호 <tmdgh717@naver.com>
2025-01-03 13:19:14 +0900

    StringBuilder - 가변 String

---
+++ src/lang/string/builder/StringBuilderMain.java
@@ -0,0 +1,23 @@
+package lang.string.builder;
+
+public class StringBuilderMain {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("A").append("B").append("C").append("D");
+        System.out.println("sb = " + sb);
+
+        sb.insert(4, "Java");
+        System.out.println("insert = " + sb);
+
+        sb.delete(4, 8);
+        System.out.println("delete = " + sb);
+
+        sb.reverse();
+        System.out.println("reverse = " + sb);
+
+        //StringBuilder -> String
+        String string = sb.toString();
+        System.out.println("string = " + string);
+    }
+}
