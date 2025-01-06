f57464decaa001e4e8e29a7f3e1adc6a153a70ff
승호 <tmdgh717@naver.com>
2025-01-06 10:36:41 +0900

    래퍼 클래스 - 기본형의 한계1

---
+++ src/lang/wrapper/MyIntegerMethodMain1.java
@@ -0,0 +1,14 @@
+package lang.wrapper;
+
+public class MyIntegerMethodMain1 {
+    public static void main(String[] args) {
+        MyInteger integer = new MyInteger(10);
+        int compare = integer.compareTo(5);
+        int compare1 = integer.compareTo(10);
+        int compare2 = integer.compareTo(20);
+
+        System.out.println("compare = " + compare);
+        System.out.println("compare1 = " + compare1);
+        System.out.println("compare2 = " + compare2);
+    }
+}
