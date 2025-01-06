b5800d4528b2c6eb8abdb1e22edee86bd4b21615
승호 <tmdgh717@naver.com>
2025-01-06 16:11:22 +0900

    섹션 6. Enum 연습 문제1

---
+++ src/enumeration/test/ex1/AuthGradeMain1.java
@@ -0,0 +1,16 @@
+package enumeration.test.ex1;
+
+public class AuthGradeMain1 {
+    public static void main(String[] args) {
+        AuthGrade[] grades = AuthGrade.values();
+        for (AuthGrade grade : grades) {
+            printGrade(grade);
+        }
+    }
+
+    private static void printGrade(AuthGrade grade) {
+        System.out.println("grade= " + grade.name()
+                + ", level= " +grade.getLevel()
+                + ", 설명= " + grade.getDescription());
+    }
+}
