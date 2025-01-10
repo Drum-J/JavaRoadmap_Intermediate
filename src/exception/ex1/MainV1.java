a20f6e9b326d317d08143e222aa9c01b29f287d7
승호 <tmdgh717@naver.com>
2025-01-10 17:15:42 +0900

    예외 처리가 필요한 이유2 - 오류 상황 만들기

---
+++ src/exception/ex1/MainV1.java
@@ -0,0 +1,37 @@
+package exception.ex1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainV1 {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))
+        ) {
+
+            NetworkServiceV1_1 networkService = new NetworkServiceV1_1();
+
+            while (true) {
+                writer.write("전송할 문자: ");
+                writer.flush();
+
+                String input = reader.readLine();
+                if (input.equals("exit")) {
+                    break;
+                }
+
+                networkService.sendMessage(input);
+                writer.write("\n");
+                writer.flush();
+            }
+
+            writer.write("프로그램을 정상 종료합니다.");
+            writer.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
